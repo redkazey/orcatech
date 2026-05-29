7034307a1cbd8e3d2cfe4a19e4f5876a49950ac5
redkazey <redkazey@gmail.com>
2026-05-29 13:18:11 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// ⚠️ DADOS SECRETOS - NÃO ENVIE ESSE ARQUIVO PARA NINGUÉM ⚠️
+const GITHUB_CONFIG = {
+  usuario: "redkazey",         
+  repositorio: "orcatech",     
+  nomeArquivo: "historico_orcamentos.json",
+  token: "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" 
+};
+
